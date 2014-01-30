88cf2a4f3683aefbea19914927b52b2d75ddd2d8
TheSunri5e <kestix@freenet.de>
2014-01-30 09:15:36 +0100

    Ingos Daten entfernt

--- SFBotyConsole/Program.cs
+++ SFBotyConsole/Program.cs
@@ -10,8 +10,7 @@ namespace SFBotyConsole {
 	class Program {
 
 		static void Main(string[] args) {
-			AccountSettings settings = new AccountSettings("Myrkai", "c3be50574698bc2a4dc566b4a42f7fca", "s23");
-			//AccountSettings settings = new AccountSettings("Myrkai", "c3be50574698bc2a4dc566b4a42f7fca", "xchar");
+			AccountSettings settings = new AccountSettings("nickname", "Hash", "Server");
 			//settings.BuyBeer = true;
 			//settings.PerformQuesten = false;
 			//settings.PerformStadtwache = false;
