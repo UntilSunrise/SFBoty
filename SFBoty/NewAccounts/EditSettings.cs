6077a6ac31efceaa0aff107c74a3ebfd9b7f1b7a
Myrkai <i.diesner@web.de>
2014-02-11 10:32:07 +0100

    Clonen von den AccountSettings ermöglicht

---
+++ SFBoty/NewAccounts/EditSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SFBotyCore.Mechanic.Account;
+
+namespace SFBoty.NewAccounts {
+	public partial class EditSettings : Form {
+		private AccountSettings Setting;
+		private AccountSettings Clone;
+		
+		public EditSettings(AccountSettings s) {
+			InitializeComponent();
+			Setting = s;
+			Clone = Setting.Clone();
+
+			Clone.Username += "Test";
+		}
+
+		private void btnSave_Click(object sender, EventArgs e) {
+			Setting.SetSettings(Clone);
+		}
+	}
+}
---
+++ SFBoty/NewAccounts/NewAccount.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SFBoty.NewAccounts {
+	partial class NewAccount {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.btnOk = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.txtNick = new System.Windows.Forms.TextBox();
+			this.txtHash = new System.Windows.Forms.TextBox();
+			this.txtServer = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnOk
+			// 
+			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnOk.Location = new System.Drawing.Point(197, 91);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.TabIndex = 0;
+			this.btnOk.Text = "OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnCancel.Location = new System.Drawing.Point(15, 91);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 1;
+			this.btnCancel.Text = "Abbruch";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// txtNick
+			// 
+			this.txtNick.Location = new System.Drawing.Point(91, 13);
+			this.txtNick.Name = "txtNick";
+			this.txtNick.Size = new System.Drawing.Size(181, 20);
+			this.txtNick.TabIndex = 2;
+			// 
+			// txtHash
+			// 
+			this.txtHash.Location = new System.Drawing.Point(91, 39);
+			this.txtHash.Name = "txtHash";
+			this.txtHash.Size = new System.Drawing.Size(181, 20);
+			this.txtHash.TabIndex = 3;
+			// 
+			// txtServer
+			// 
+			this.txtServer.Location = new System.Drawing.Point(91, 65);
+			this.txtServer.Name = "txtServer";
+			this.txtServer.Size = new System.Drawing.Size(181, 20);
+			this.txtServer.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Nickname:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(73, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "PaswortHash:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 68);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Server:";
+			// 
+			// NewAccount
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 122);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtServer);
+			this.Controls.Add(this.txtHash);
+			this.Controls.Add(this.txtNick);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnOk);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "NewAccount";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "NewAccount";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.TextBox txtNick;
+		private System.Windows.Forms.TextBox txtHash;
+		private System.Windows.Forms.TextBox txtServer;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
