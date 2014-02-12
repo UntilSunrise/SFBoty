e0a976fdf8e28add76159b6b3b77a3347bd090e1
TheSunri5e <kestix@freenet.de>
2014-02-12 18:23:16 +0100

    Spende auch am Tag des Botstarts.

--- SFBotyCore/Mechanic/Account/Account.cs
+++ SFBotyCore/Mechanic/Account/Account.cs
@@ -116,7 +116,7 @@ namespace SFBotyCore.Mechanic.Account {
 			HasJoinAttack = false;
 			HasJoinDefence = false;
 
-			LastDonateTime = DateTime.Now;
+			LastDonateTime = DateTime.Now.AddDays(-1);
 		}
 
 		public void Logout() {
