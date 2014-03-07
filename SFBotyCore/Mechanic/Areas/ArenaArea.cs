1252da5c38f78ba33a6d5f557f1fc8683aedf7fa
TheSunri5e <kestix@freenet.de>
2014-03-07 09:56:02 +0100

    Typenbezeichnung angepasst.

--- SFBotyCore/Mechanic/Areas/ArenaArea.cs
+++ SFBotyCore/Mechanic/Areas/ArenaArea.cs
@@ -91,7 +91,7 @@ namespace SFBotyCore.Mechanic.Areas {
 					if (Account.Settings.AttackEnemyBetweenRange) {
 						try {
 							List<int> filteredEnemys = new List<int>();
-							foreach (var id in HoFCharacters.Where(x => x.Value.Level >= myLevel - levelDifference && x.Value.Level <= myLevel + levelDifference)) {
+							foreach (KeyValuePair<int, HoFCharacter> id in HoFCharacters.Where(x => x.Value.Level >= myLevel - levelDifference && x.Value.Level <= myLevel + levelDifference)) {
 								filteredEnemys.Add(id.Key);
 							}
 							myRandomEnemy = filteredEnemys[random.Next(0, filteredEnemys.Count)];
