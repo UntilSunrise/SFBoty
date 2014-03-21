e75323a8c93615576dbead9bfdff050dad327ed8
TheSunri5e <kestix@freenet.de>
2014-03-21 02:34:46 +0100

    Linux-Fix: Ich habe keine Ahnung warum das hier nötig ist, unter Win funktioniert es ohne den zusätzlichen Request. Lässt man den Fix unter Linux raus, bekommt man den Error, dass minTimeToUseItem kleiner als maxTimeToUseItem sei. Dies geschieht aber auch nur an dieser Stelle in der CharScreenArea.
    Mögliche Ursache: Problem unter Mono.

--- SFBotyCore/Mechanic/Areas/CharScreenArea.cs
+++ SFBotyCore/Mechanic/Areas/CharScreenArea.cs
@@ -169,6 +169,7 @@ namespace SFBotyCore.Mechanic.Areas {
 
 					if (bpItemIsBetter) {
 						RaiseMessageEvent(String.Format("Lege Rucksack-Item im Slot: {0} an.", bpItem.InventoryID));
+						s = SendRequest(ActionTypes.JoinCharacter);
 						ThreadSleep(Account.Settings.minTimeToUseItem, Account.Settings.maxTimeToUseItem);
 						s = SendRequest(ActionTypes.ItemAction + bpItem.InventoryID + "%3B1%3B-1");
 					}
